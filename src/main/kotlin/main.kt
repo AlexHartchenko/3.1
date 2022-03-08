f3383b177d6c88edd35238a50078e8ed9ab6dd0c
Alex Hartchenko <aj.tomsk@mail.ru>
2022-03-08 18:44:10 +0700

    initial commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ package ru.netology
 
 fun main() {
 
-    val absenceTime = 7_000
+    val absenceTime = 70_000
 
     agoToText(absenceTime)
 }
@@ -29,8 +29,8 @@ private fun agoToText(absenceTime: Int) {
 private fun minutes(time: Int): String {
     return when (time) {
         1, 21, 31, 41, 51 -> "минуту"
-        in 2..4 -> "минуты"
-        in 5..20, in 22..30, in 32..40, in 42..50, in 52..60 -> "минут"
+        in 2..4, in 22..24, in 32..34, in 42..44, in 52..54 -> "минуты"
+        in 5..20, in 25..30, in 35..40, in 45..50, in 55..60 -> "минут"
         else -> ""
     }
 }
